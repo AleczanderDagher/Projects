8b14d287685b46baf6d2b94e377be77fd5d58fc2
AleczanderDagher <90352936+AleczanderDagher@users.noreply.github.com>
2023-01-03 15:18:55 -0500

    Update Assignment2-2.py

--- Python Class/Assignment 2/Assignment2-2.py
+++ Python Class/Assignment 2/Assignment2-2.py
@@ -1,5 +1,10 @@
 import constant
 
+# Please create a python program that will ask the user for the gas mileage on a given car (i.e. 24 
+# MPG) and how far they plan on traveling (i.e. 125 miles). Calculate the expenses incurred on a trip 
+# with gas costing $3.49 per gallon. 
+# a. Display the end result back to the user on how much the trip will cost. 
+
 # Enter gas mileage of car and travel distance
 gasMileage = float(input("Enter gas mileage of car (miles per gallon): "))
 travelDistance = float(input("Enter how far you want to travel (miles): "))
